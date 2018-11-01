d9a295e073601a24588e698a8d908df6e9e6ed49
dmitriy <paradoxxru@list.ru>
2018-11-01 14:06:26 +0300

    mysite final

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+		let gulp = require('gulp-v4'); // Ядро Gulp 
+        let less = require('gulp-less'); //Less компилятор
+        let LessAutoprefix = require('less-plugin-autoprefix'); //Автопрефиксер
+        let autoprefix = new LessAutoprefix({ 
+            browsers: ['last 2 versions'] 
+        }); //Опции автопрефиксера
+        let cleanCSS = require('gulp-clean-css'); //Конкатенация CSS
+
+
+        function html () {
+            console.log("html has been updated");
+            return gulp.src('./site/**/*.html')
+                .pipe(gulp.dest('../public/site'));
+            
+        }
+        
+
+		function images() {
+        console.log("images has been updated");
+        return gulp.src('./site/images/**/*.*')
+            .pipe(gulp.dest('../public/site/images'));
+        }
+        function css () {
+            console.log("css has been updated");
+            return gulp.src('./site/css/**/*.css')
+                .pipe(gulp.dest('../public/site/css'));
+        }
+        
+
+        gulp.task('images', images);
+        gulp.task('html', html);
+        gulp.task('css', css);
+        gulp.task('assets', assets);
+
+        //Серия команд
+        let all = gulp.series(images, html, css, assets, lessBuild);
+        gulp.task('default', all)
+        function assets () {
+            console.log("assets has been updated");
+            return gulp.src([
+                './site/**/*.*', 
+                '!./site/**/*.js', 
+                '!./site/**/*.css', 
+                '!./site/**/*.png',
+                '!./site/**/*.html',
+                '!./site/**/*.less',
+                '!./site/**/*.jpg',
+                '!./site/**/*.jpeg',
+                '!./site/**/*.svg'
+                ])
+                .pipe(gulp.dest('../public/site'));
+            
+        }
+
+        
+		
+		function lessBuild () {
+            //Обработка less
+            console.log("less has been compiled");
+            return gulp.src('./site/css/**/bundle.less')
+            //Автопрефиксы
+            .pipe(less({
+                plugins: [autoprefix]
+            }))
+            .on('error', handleError) // Отлов ошибок без выхода из приложения
+                //Конкатенация
+                .pipe(cleanCSS()) 
+                .pipe(gulp.dest('../public/site/css'))
+        }
+// Отлов ошибок без выхода из приложения
+        function handleError(err) {
+            console.log(err.toString());
+            this.emit('end');
+        }
+        gulp.task('less', lessBuild);
